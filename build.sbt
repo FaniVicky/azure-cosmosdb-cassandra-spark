0693ab5a30bd1b042674b4901e6cdb85999adbd6
Fani <fpilla@walmart.com>
2019-07-09 10:16:10 -0700

    Adding spark session

--- build.sbt
+++ build.sbt
@@ -2,6 +2,15 @@ name := "azure-cosmosdb-cassandra-spark"
 
 version := "0.1"
 
-scalaVersion := "2.13.0"
+scalaVersion := "2.11.8"
 
-libraryDependencies += "com.microsoft.azure" % "azure-cosmosdb-spark_2.4.0_2.11" % "1.3.5"
+libraryDependencies ++= {
+  Seq(
+    "com.microsoft.azure" % "azure-cosmosdb-spark_2.4.0_2.11" % "1.3.5",
+    "org.apache.spark" % "spark-core_2.11" % "2.3.0" % "provided",
+    "org.apache.spark" % "spark-sql_2.11" % "2.3.0" % "provided"
+  )
+}
+
+dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.7"
+
--- src/main/scala/AzureCosmosDbSparkConnector.scala
+++ src/main/scala/AzureCosmosDbSparkConnector.scala
@@ -1,6 +1,6 @@
-import com.microsoft.azure.cosmosdb.spark.schema._
-import com.microsoft.azure.cosmosdb.spark._
 import com.microsoft.azure.cosmosdb.spark.config.Config
+import org.apache.spark.sql.SparkSession
+
 
 object AzureCosmosDbSparkConnector {
 
@@ -9,10 +9,20 @@ object AzureCosmosDbSparkConnector {
       "Endpoint" -> "https://qa-wakanda-ca.documents.azure.com:443/",
       "Masterkey" -> "2JGov0jL83tVhmaKbYAcfGKX0WW6qLQmUlzZRrPZA1M36ev01poUg2cQmYtQ0GHTnhMsfwpF1prqF8PHItQ40A==",
       "Database" -> "bashenga",
-      "Collection" -> "fulfillment-nodes"
+      "Collection" -> "fulfillment-nodes",
+      "query_custom" -> "SELECT c.sId FROM c WHERE c.et = 'nodeCreated/1'"
     ))
 
+    val spark = SparkSession.builder()
+      .appName("AzureCosmosDBRead")
+      .master("local[1]")
+      .getOrCreate()
+
+    import com.microsoft.azure.cosmosdb.spark.schema._
     val df = spark.read.cosmosDB(readConfig)
+
+    df.explain()
+    df.show(3)
     df.count()
   }
 }
